05ba8a4cef23f5164d1580eee226c63070cab722
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-08-01 15:31:47 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/ramrk.png';
+import profileImage from '../../assets/profilepic.png';
 
 const About = () => {
   return (
