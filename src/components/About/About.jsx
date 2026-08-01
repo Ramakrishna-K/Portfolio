c528a03cdf0ca5c495187ec02e5950052144fa5c
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-08-01 15:51:30 +0530

    Update profile image source in About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/picporfile.png';
+import profileImage from '../../assets/picprofile1.png';
 
 const About = () => {
   return (
