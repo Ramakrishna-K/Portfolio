a2f5ae5e3484455c95b91db66ddb5dae684d63fb
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-09-20 16:14:22 +0530

    Update Certifications.jsx

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -5,7 +5,7 @@ const Certifications = () => {
   return (
     <section
       id="certifications"
-      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
+      className="py-24 px-[6vw] md:px-[8vw] lg:px-[10vw] font-sans"
     >
       {/* Section Title */}
       <div className="text-center mb-16">
