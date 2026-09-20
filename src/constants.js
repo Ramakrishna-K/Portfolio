f12ec8fd6a8a5ed7fe2997c583d227e8035a6846
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-09-20 16:05:26 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -212,8 +212,7 @@ export const certifications = [
   desc: "Completed an intensive certification focused on Generative AI, Agentic AI systems, Python, LangChain, AI workflows, and full-stack AI application development.",
   link: "https://drive.google.com/file/d/1V-xyJEOFAKb8rmXygv07KM-J4Im4xWrf/view?usp=drive_link"
   
-}
-{
+},
 {
   id: 3,
   title: "All India Hackathon (AIH) 2026",
